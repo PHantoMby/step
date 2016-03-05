342a2f323ed1479a74a34561fb3d934e5882811c
PHantoMby <phantom@nsys.by>
2016-03-05 12:17:50 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,1 +1,9 @@
-0
+CONFIG = {
+    'working_dir': '/home/box/web',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=16',
+        '--timeout=60',
+        'hello:simple_app',
+    ),
+}
