2c6adf29530e7dec82e91c96271d424eacf89ae6
PHantoMby <phantom@nsys.by>
2016-03-05 12:16:56 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-123
+def simple_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return [environ['QUERY_STRING'].replace('&', '\n')]
