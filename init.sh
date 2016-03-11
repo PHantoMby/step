5b5b826210705aa177dcbaf2358f34a83c908790
PHantoMby <phantom@nsys.by>
2016-03-12 00:23:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,16 @@
+sudo apt-get update
+sudo apt-get install nginx
+sudo apt-get install python3-pip
+sudo pip3 install gunicorn
+sudo pip3 install django
+sudo pip3 install pymysql
+sudo cp /home/box/web/nginx.conf /etc/nginx/
+sudo pip install pymysql
+sudo /etc/init.d/mysql restart
+mysql -u root -e "create database box_django"
+sudo python3 /home/box/web/ask/manage.py makemigrations
+sudo python3 /home/box/web/ask/manage.py migrate
+
 #sudo unlink /etc/nginx/sites-enabled/default
 #sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 #sudo nginx -t && sudo /etc/init.d/nginx restart
@@ -11,9 +24,9 @@
 # sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
 # sudo /etc/init.d/gunicorn restart
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:application
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo rm /etc/nginx/sites-enabled/default
+#sudo /etc/init.d/nginx restart
+#sudo gunicorn -b 0.0.0.0:8080 hello:application
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
